474c0d067520a501fcb287a788ab6a51b46381e0
CollinDex <obetscollins@gmail.com>
2024-10-19 01:36:31 +0100

    chore: project setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // All JS, TS, JSX, and TSX files in src/
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
